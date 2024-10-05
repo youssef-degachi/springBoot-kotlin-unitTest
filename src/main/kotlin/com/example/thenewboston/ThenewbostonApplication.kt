8787e61aa8dc137221d0685e0fc41a3ca6fa7ca9
youssef-degachi <youssefdegachi0@gmail.com>
2024-10-05 13:45:43 +0100

    create the project

---
+++ src/main/kotlin/com/example/thenewboston/ThenewbostonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.thenewboston
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThenewbostonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThenewbostonApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/thenewboston/ThenewbostonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.thenewboston
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThenewbostonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
