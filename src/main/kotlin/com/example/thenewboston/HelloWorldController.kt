32bbe89a268fd2e53197697ca15d8544148922ac
youssef-degachi <youssefdegachi0@gmail.com>
2024-10-05 14:22:38 +0100

    finsh the first video

--- src/main/kotlin/com/example/thenewboston/HelloWorldController.kt
+++ src/main/kotlin/com/example/thenewboston/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package com.example.thenewboston
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,4 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/hello")
 class HelloWorldController {
 
+        @GetMapping()
+        fun helloWorld(): String {
+            return "Hellow, this is a rest endpoint";
+        }
+        @GetMapping("youssef")
+         fun helloYoussef(): String =  "Hello youssef"
 }
